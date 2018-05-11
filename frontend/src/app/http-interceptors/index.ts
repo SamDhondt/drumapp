6499f91e2600be0c056692c8159d61d92cefda4f
Sam Dhondt <sam.dhondt.w1599@student.hogent.be>
2018-05-11 17:21:44 +0200

    More heroku changes

--- frontend/src/app/http-interceptors/index.ts
+++ frontend/src/app/http-interceptors/index.ts
@@ -1,6 +1,9 @@
+/* "Barrel" of Http Interceptors */
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthenticationInterceptor } from './AuthenticationInterceptor';
+import { BaseUrlInterceptor } from './base-url.interceptors';
 
+/** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
   {
     provide: HTTP_INTERCEPTORS,
@@ -8,7 +11,10 @@ export const httpInterceptorProviders = [
     multi: true
   }
 ];
-
-export const basehttpInterceptorProviders = [ {
-provide: HTTP_INTERCEPTORS, useClass: BaseUrlInterceptor, multi: true
-} ];
+export const basehttpInterceptorProviders = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: BaseUrlInterceptor,
+    multi: true
+  }
+];
